77f6c793878dc0ce63b15e34a11d212a68f4bec4
chrisf <3920981+cjfullerton@users.noreply.github.com>
2020-04-02 15:28:18 +0100

    fixed bug in driver.pyw

--- driver.py
+++ driver.py
@@ -1,4 +1,4 @@
 import io
 
 data = io.get_data(20, "physics")
-[rep_data, name, creation_date, last_push_date, commit_page_data, has_next_page, commits] = io_test.process_aquired_data(data)
+[rep_data, name, creation_date, last_push_date, commit_page_data, has_next_page, commits] = io.process_aquired_data(data)
