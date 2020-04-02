3575d37ea7914f7f9efe14eb9118dd3209513700
Dan Hobley <dan.hobley@gmail.com>
2020-04-02 13:18:48 +0100

    create datetime obj

--- io.py
+++ io.py
@@ -1,6 +1,7 @@
 # remember to make an HTTPDigestAuth object!
 
 import requests, json
+from datetime import datetime
 from requests.auth import HTTPDigestAuth
 
 # headers = {'Authorization': 'Bearer KEY'}
@@ -51,3 +52,14 @@ for rep in aquired_repos:
     commit_page_data = rep_data['ref']['target']['history']
     has_next_page = commit_page_data['pageInfo']['hasNextPage']
     commits = commit_page_data['edges']  # this is the list of <=100 commits
+
+
+def convert_datetime(datetime_str):
+    yr = int(datetime_str[:4])
+    mo = int(datetime_str[5:7])
+    da = int(datetime_str[8:10])
+    hr = int(datetime_str[11:13])
+    mi = int(datetime_str[14:16])
+    se = int(datetime_str[17:19])
+    dt = datetime(yr, mo, da, hr, mi, se)
+    return dt
