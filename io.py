d61ee4858331296571dcebf4f234436ac570053c
Dan Hobley <dan.hobley@gmail.com>
2020-04-02 09:33:11 +0100

    sketch io scheme

--- io.py
+++ io.py
@@ -1,1 +1,13 @@
+# remember to make an HTTPDigestAuth object!
+
 import requests
+from requests.auth import HTTPDigestAuth
+
+# auth = HTTPDigestAuth('User', 'key')
+
+r = requests.get('https://api.github.com/repositories', params={'since': 100},
+                 auth=auth)
+print(r.json()[0]['commits_url'])
+
+rcommits = requests.get(r.json()[0]['commits_url'][:-6])
+print(r.json()[0].keys())
