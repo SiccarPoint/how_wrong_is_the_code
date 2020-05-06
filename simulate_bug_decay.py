55c6c5a2ba8b90b6362bff1ca83db324aa75f8bd
Dan Hobley <dan.hobley@gmail.com>
2020-05-06 10:51:40 +0100

    Permit run until a fixed time, not number of bugs

--- simulate_bug_decay.py
+++ simulate_bug_decay.py
@@ -19,7 +19,8 @@ def generate_bug(mean=22., std=6.):
 
 
 def advance_finding_bugs(current_time, bug_creation_time,
-                         total_number_of_bugs_to_find, event_stack,
+                         max_total_number_of_bugs_to_find,
+                         event_stack,
                          times_of_bug_finds):
     """
     Move forward through an event_stack from time current_time to time
@@ -31,9 +32,8 @@ def advance_finding_bugs(current_time, bug_creation_time,
            < bug_creation_time):
         current_time += event_stack.time_until_next_bug(current_time)
         times_of_bug_finds.append(event_stack.advance())
-        if len(times_of_bug_finds) == total_number_of_bugs_to_find:
+        if len(times_of_bug_finds) == max_total_number_of_bugs_to_find:
             raise ValueError
-
 
 
 class bug():
@@ -106,7 +106,8 @@ class event_stack():
 
     def time_until_next_bug(self, current_time):
         """
-        Raises an IndexError if the event stack lacks any bugs
+        Raises an IndexError if the event stack lacks any bugs.
+
         Returns
         -------
         elapsing_time : float
@@ -122,14 +123,21 @@ class event_stack():
         """
         return self._events.pop(0)
 
-
     @property
     def events(self):
         return self._events
 
-if __name__ == "__main__":
-    total_number_of_bugs_to_find = 250
-    bug_lifetime_parameter = 0.0001
+
+def run_a_model(max_number_of_bugs_to_find, max_number_of_commits_permissable,
+                bug_lifetime_parameter,
+                generate_bug_params, number_of_starting_bugs):
+    """
+    Here, the funcion runs until either the bug count reaches
+    max_number_of_bugs_to_find, or the "time" exceeds
+    max_number_of_commits_permissable.
+    """
+    #total_number_of_bugs_to_find = 250
+    #bug_lifetime_parameter = 0.0001
     # ^dropping this moves us away from a single production line.
     # We drop back from it - but staying parallel - as blp < ~0.01
     # We start to lose parallelism as blp < 0.0005, assoc. w a slow period
@@ -137,12 +145,13 @@ if __name__ == "__main__":
     # In these cases, we are just delayed in approaching the (still identical)
     # background rate.
     # (I don't think our repos are commonly in this zone)
-    number_of_starting_bugs = 50
+    #number_of_starting_bugs = 50
     # The more bugs there are, the more pronounced the kinking the accumulation
     variable_lifetime = False
     times_of_bug_finds = []
     estack = event_stack()
     current_time = 0.
+    time_is_exceeded = False
 
     # set up the initial bugs:
     for i in range(number_of_starting_bugs):
@@ -151,23 +160,30 @@ if __name__ == "__main__":
         print("Adding bug, lifetime:", a_bug.lifetime)
         estack.add_a_bug(a_bug)
     # now run the model
-    for time_from_now_to_bug_creation in generate_bug():
+    for time_from_now_to_bug_creation in generate_bug(generate_bug_params[0],
+                                                      generate_bug_params[1]):
         # prepare the next bug to be added:
         bug_creation_time = current_time + time_from_now_to_bug_creation
-        a_bug = bug(bug_lifetime_parameter, bug_creation_time,
-                    variable_lifetime=variable_lifetime)
-        # add it
-        estack.add_a_bug(a_bug)
+        if bug_creation_time > max_number_of_commits_permissable:
+            bug_creation_time = max_number_of_commits_permissable
+            time_is_exceeded = True
+        else:
+            a_bug = bug(bug_lifetime_parameter, bug_creation_time,
+                        variable_lifetime=variable_lifetime)
+            # add it
+            estack.add_a_bug(a_bug)
         # now advance, finding bugs, until we need to generate a new bug:
         try:
             advance_finding_bugs(current_time, bug_creation_time,
-                                 total_number_of_bugs_to_find, estack,
+                                 max_number_of_bugs_to_find,
+                                 estack,
                                  times_of_bug_finds)
         except ValueError:
             break
+        if time_is_exceeded:
+            break
         # Move through rest of tstep to the point where the bug is created:
         current_time = bug_creation_time
-
 
     plt.figure(1)
     plt.plot(times_of_bug_finds, list(range(len(times_of_bug_finds))))
@@ -177,3 +193,26 @@ if __name__ == "__main__":
     plt.hist(np.diff(times_of_bug_finds), bins='auto')
     plt.xlabel('Interval between bug finds')
     plt.ylabel('Number of occurrences')
+    # plt.figure(3)
+    # # mock up our key data fig
+    # number_caught = np.searchsorted(times_of_bug_finds, time_elapsed)
+    # plt.plot(time_elapsed, number_caught / time_elapsed, 'x')
+    # plt.xlabel('Commits in repo')
+    # plt.ylabel('Apparent bug find rate')
+
+    return times_of_bug_finds
+
+
+if __name__ == "__main__":
+    doi_bug_commit_distn = np.loadtxt('doiorg_total_commits_for_each_repo.txt')
+    for rate in (0.0001, ):# (0.0001, 0.001, 0.01):
+        for num_start_bugs in (250, ):#(0, 50, 250):
+            for i in range(10):
+                # draw a plausible repo length:
+                repo_len = np.random.choice(doi_bug_commit_distn)
+                # repo_len = 1000
+                run_a_model(10000, repo_len, rate, (10., 3.), num_start_bugs)
+                # (10,3) very approx for doi.org
+                # An early cutoff before repo len without reaching num_bugs
+                # indicates the model did not find more bugs in the remaining
+                # interval
