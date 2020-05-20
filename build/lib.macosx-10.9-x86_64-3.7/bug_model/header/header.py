5e2431b63fef8d70908483506f25a0cd0b845081
Dan Hobley <dan.hobley@gmail.com>
2020-05-20 12:14:16 +0100

    clean build files

--- build/lib.macosx-10.9-x86_64-3.7/bug_model/header/header.py
+++
@@ -1,1 +0,0 @@
-HEADER = {'Authorization': "Bearer ed3c64566350e51ffcf580580084ed9161a65a44"}  
--- build/lib.macosx-10.9-x86_64-3.7/bug_model/utils.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-def moving_average(a, n=10):
-    ret = np.cumsum(a, dtype=float)
-    ret[n:] = ret[n:] - ret[:-n]
-    return ret[n - 1:] / n
