44c4a98e41f0fdf22cc1f0f360795c42c4d874f2
Dan Hobley <dan.hobley@gmail.com>
2020-05-22 13:00:26 +0100

    add a useful plotter

---
+++ dakota/dakota_plotters.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from plotnine import *
+from matplotlib.pyplot import plot, figure, show
+
+DATFILE = 'dakota_gridtraverse3.dat'
+INPARAMS = ["R", "S", "F"]
+OUTPARAMS = ["rmse","diff_std_at_low_commits"]
+
+df = pd.read_csv(DATFILE, engine='python', delim_whitespace=True)
+df = df.set_index(OUTPARAMS).drop(columns=["interface"])
+
+(
+    ggplot(df.reset_index(),
+           aes(x=INPARAMS[1], y=INPARAMS[2], color=OUTPARAMS[0]))
+    + geom_point()
+    + scale_color_continuous(limits=(0., 0.5))
+)
